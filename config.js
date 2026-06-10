51eaccf0ad3f4eefc45977da24ee4f6a81b0c014
Napoles FC <napolesfc.ec@gmail.com>
2026-06-10 12:33:35 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // ASEGÚRATE DE QUE LA URL TERMINE EN /exec
-    URL_GAS: "https://script.google.com/macros/s/AKfycbybGi_p22bkyO-CHnSjlZEQSmWETPjqA0zZv-m2Ox9E2Jvec1b-XnPySv6259wfI48/exec"
+    URL_GAS: "https://script.google.com/macros/s/AKfycbyXFPPjL4hbjiEeNYRuTA4eDy0iFHw1GC4_RslcVwy1qIhrHnm5JqHpwc_ttJXZFz0y/exec"
 };
 
 // ESTO ES PARA PROBAR SI EL ARCHIVO CONFIG.JS SE CARGA BIEN
